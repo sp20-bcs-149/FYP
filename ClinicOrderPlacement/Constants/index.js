fba1dd96823390beeffbbeeed058acfeff2b00d2
M Subhan Khan <126487035+MSubhanKhan786000@users.noreply.github.com>
2023-09-26 12:03:23 -0700

    OrderVaccine

---
+++ ClinicOrderPlacement/Constants/index.js
@@ -0,0 +1,150 @@
+export const categories = [
+    {
+        id: 1,
+        name: 'Vaccine',
+        image:  require('../assets/Images/HepatitisB.png'),
+    },
+    {
+        id: 2,
+        name: 'Vaccine',
+        image:  require('../assets/Images/HepatitisB.png'),
+    },
+    {
+        id: 3,
+        name: 'Vaccine',
+        image:  require('../assets/Images/HepatitisB.png'),
+    },
+    {
+        id: 4,
+        name: 'Vaccine',
+        image:  require('../assets/Images/HepatitisB.png'),
+    },
+    {
+        id: 5,
+        name: 'Vaccine',
+        image:  require('../assets/Images/HepatitisB.png'),
+    },
+    {
+        id: 7,
+        name: 'Vaccine',
+        image:  require('../assets/Images/HepatitisB.png'),
+    },
+
+]
+
+
+export const featured = {
+    id: 1,
+    title: 'Hot and Spicy',
+    description: 'soft and tender fried chicken',
+    restaurants: [
+        {
+            id: 1,
+            name: 'Polio',
+            image:  require('../assets/Images/HepatitisB.png'),
+            description: 'Hot and spicy pizza',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Vaccine',
+            dishes: [
+                {
+                   id: 1,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+                {
+                   id: 2,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+                {
+                   id: 3,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+            ]
+    
+        },
+        {
+            id: 2,
+            name: 'Papa Johns',
+            image:  require('../assets/Images/HepatitisB.png'),
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                   id: 1,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+                {
+                   id: 2,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+                {
+                   id: 3,
+                   name: 'pizza',
+                   description: 'cheezy garlic pizza',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+            ]
+    
+        },
+        {
+            id: 3,
+            name: 'Papa Johns',
+            image:  require('../assets/Images/HepatitisB.png'),
+            description: 'Hot and spicy pizzas',
+            lng: -85.5324269,
+            lat: 38.2145602,
+            address: '434 second street',
+            stars: 4,
+            reviews: '4.4k',
+            category: 'Fast Food',
+            dishes: [
+                {
+                   id: 1,
+                   name: 'Hepatitis B',
+                   description: 'Vaccine of Hepatitis B',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+                {
+                   id: 2,
+                   name: 'Hepatitis A',
+                   description: 'Vaccine of Hepatitis A',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+                {
+                   id: 3,
+                   name: 'HIB',
+                   description: 'Vaccine of HIB',
+                   price: 10,
+                   image:  require('../assets/Images/HepatitisB.png')
+                },
+            ]
+    
+        }
+    ]
+}
