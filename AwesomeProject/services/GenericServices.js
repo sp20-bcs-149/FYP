59c1a9628c08649cf63a830078c4ac957356e6da
Adil Hussain <113510931+Adil-Hussain133@users.noreply.github.com>
2023-10-17 10:58:12 +0500

    .

--- AwesomeProject/services/GenericServices.js
+++ AwesomeProject/services/GenericServices.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://192.168.10.12:4000/";
-
+axios.defaults.baseURL = "http://10.135.48.252:4000/";
 
 // axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 
